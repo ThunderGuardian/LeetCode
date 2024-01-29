8c7a63bfdd15a818f6e09114ad7801963e9bb77a
Sayeem Khan <114165523+ThunderGuardian@users.noreply.github.com>
2024-01-29 21:08:37 +0530

    Create Product_of_Array_Except_Self.java
    Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
    
    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
    
    You must write an algorithm that runs in O(n) time and without using the division operation

---
+++ Product_of_Array_Except_Self.java
@@ -0,0 +1,20 @@
+//Difficulty: Medium
+//Runtime: 1 ms
+//Memory Usage: 53.4 MB
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int arr[]=new int[nums.length];
+        int prefix =1;
+        int postfix =1;
+        for(int j=0;j<nums.length;j++){
+            arr[j]=prefix;
+            prefix=prefix*nums[j];
+        }
+        for(int i=nums.length-1;i>=0;i--)
+        {
+            arr[i]=arr[i]*postfix;
+            postfix=postfix*nums[i];
+        }
+        return(arr);
+    }
+}
