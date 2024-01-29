b730a0a4656a1a46de360a836438f612961a75b6
Sayeem Khan <114165523+ThunderGuardian@users.noreply.github.com>
2024-01-29 21:18:43 +0530

    Update Product_of_Array_Except_Self.java
    Adding comments to help revise the problem later

--- Product_of_Array_Except_Self.java
+++ Product_of_Array_Except_Self.java
@@ -7,10 +7,12 @@ class Solution {
         int arr[]=new int[nums.length];
         int prefix =1;
         int postfix =1;
+        //Putting the prefix of each element in the new array at the respective index. Hence this loop goes from the start index till the end index
         for(int j=0;j<nums.length;j++){
             arr[j]=prefix;
             prefix=prefix*nums[j];
         }
+        //And then multiplying the prefix of the respective element with its prostfix. Hence this for loop goes from the end of the nums[] till the start
         for(int i=nums.length-1;i>=0;i--)
         {
             arr[i]=arr[i]*postfix;
