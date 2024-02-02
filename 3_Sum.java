7e9ffa6239d9f58501f685a0077f5c754e64f889
Sayeem Khan <114165523+ThunderGuardian@users.noreply.github.com>
2024-02-02 21:54:48 +0530

    Create 3_Sum.java
    Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
    
    Notice that the solution set must not contain duplicate triplets.

---
+++ 3_Sum.java
@@ -0,0 +1,46 @@
+//Difficulty: Medium
+//Runtime: 33 ms
+//Memory Usage: 51.2 MB
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<Integer> w = new ArrayList<Integer>();
+        List<List<Integer>> arr = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length-2;i++){
+            if(i==0 || (i>0 && (nums[i]!=nums[i-1]))){
+            int j=i+1;
+            int k=nums.length-1;
+            
+
+            while(j<k){
+                int result=nums[i]+nums[j]+nums[k];
+                if(result==0){
+                
+                   arr.add(Arrays.asList(nums[i], nums[j],nums[k]));
+                   //System.out.println("ArrayList= "+arr);
+
+                   while(j<k && nums[j]==nums[j+1]){
+                       j++;
+                   }
+                   while(j<k && nums[k]==nums[k-1]){
+                       k--;
+                   }
+                   j++;
+                   k--;
+
+                }
+                else if(result>0){
+                    k--;
+                }
+                else if(result<0){
+                    j++;
+                }
+
+            }
+            }
+        }
+        return(arr);
+    }
+}
